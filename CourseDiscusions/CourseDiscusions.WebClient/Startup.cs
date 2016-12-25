538c730a7e13b6d2cbbc9d076a2e2a0b43d2299d
slav. <slav_@DESKTOP-JQR7HUT>
2016-12-25 18:01:55 +0200

    Initial Commit

---
+++ CourseDiscusions/CourseDiscusions.WebClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CourseDiscusions.WebClient.Startup))]
+namespace CourseDiscusions.WebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
